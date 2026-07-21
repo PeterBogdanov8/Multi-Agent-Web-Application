44cb7f80393bce88499d91e1f2df8e299c91e000
PeterBogdanov8 <peterbogdanov.pob@gmail.com>
2026-07-21 12:05:12 +0200

    Add the SearchAgent that makes use of various search algorithms to select candidates for the job

--- backend/agent_types/agent_type.py
+++ backend/agent_types/agent_type.py
@@ -3,3 +3,5 @@ from enum import Enum
 
 class AgentType(str, Enum):
     SimulatedAnnealingAgentType = "SimulatedAnnealingAgent"
+    SearchAgentType = "SearchAgent"
+    
--- backend/main.py
+++ backend/main.py
@@ -3,6 +3,7 @@ from math import log
 from fastapi import FastAPI
 
 from agent_types.agent_type import AgentType
+from agents.search_agent import SearchAgent
 from agents.simulated_annealing_agent import SimulatedAnnealingAgent
 from candidate.candidate_loader import CandidateLoader
 
@@ -18,4 +19,7 @@ def get_single_agent_candidates(budget: int, job: str, agent_type: AgentType):
         case AgentType.SimulatedAnnealingAgentType:
             sa_agent = SimulatedAnnealingAgent(budget, job, candidates)
             solution = sa_agent.solve_problem(lambda x: log(x, 10))
+        case AgentType.SearchAgentType:
+            search_agent = SearchAgent(budget, job, candidates)
+            solution = search_agent.expected_employer_vs_employee_pay_search()
     return solution
