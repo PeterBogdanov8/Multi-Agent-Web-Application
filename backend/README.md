9af4e43dbbfbc312b19f0552c5767e6de3d0db32
PeterBogdanov8 <peterbogdanov.pob@gmail.com>
2026-07-21 11:31:11 +0200

    Add the get-single-agent-candidates endpoint that fetches the candidates chosen by the AI agent. Add the SimulatedAnnealingAgent that make use of the simulated annealing algorithm to choose candidates for a job based on the budget Add a README file to show the user how to run the project

---
+++ backend/README.md
@@ -0,0 +1,4 @@
+Follow the these steps:
+1. Open the Command Palette (`Ctrl+Shift+P`), type Python: Select Interpreter, and choose the `.venv` path.
+2. Run `pip install "fastapi[standard]"` in the terminal.
+3. Run `fastapi dev main.py` in the terminal.
---
+++ backend/main.py
@@ -0,0 +1,21 @@
+from math import log
+
+from fastapi import FastAPI
+
+from agent_types.agent_type import AgentType
+from agents.simulated_annealing_agent import SimulatedAnnealingAgent
+from candidate.candidate_loader import CandidateLoader
+
+app = FastAPI()
+
+@app.get("/get-single-agent-candidates/{budget}/{job}/{agent_type}")
+def get_single_agent_candidates(budget: int, job: str, agent_type: AgentType):
+    candidate_loader = CandidateLoader('data/candidates.json')
+    candidates = candidate_loader.get_candidates()
+    solution = None
+    
+    match agent_type:
+        case AgentType.SimulatedAnnealingAgentType:
+            sa_agent = SimulatedAnnealingAgent(budget, job, candidates)
+            solution = sa_agent.solve_problem(lambda x: log(x, 10))
+    return solution
